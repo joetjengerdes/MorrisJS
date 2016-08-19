44440b94f65ea46b5734643f45cabb32d7d2f016
Jannis Oetjengerdes <ja_oe@uni-bremen.de>
2016-08-19 14:16:20 +0200

    resize gefixxt

--- assets/js/services/SizeCalculationService.js
+++ assets/js/services/SizeCalculationService.js
@@ -10,18 +10,34 @@ function SizeCalculationService(canvas, gf) {
     canvas.width = document.body.clientWidth; //document.width is obsolete
     canvas.height = Math.max(window.innerHeight, document.body.clientHeight);
 
-    var h = canvas.height;
-    var w = canvas.height;
 
+  //  var h = canvas.height;
+  //  var w = h;
+    var canvasHeight = Math.min(window.innerHeight, document.body.clientHeight);
 
-    gamefield.spaceBetween = (h + w) / 15;
-    gamefield.circleSize = (h + w) / 120;
+    var size;
+    if(canvasHeight <= canvas.width) {
+      size = canvasHeight;
+    }
+    else {
+      size = canvas.width;
+    }
 
-    gamefield.lineWidth = (h + w) / 350;
+    //console.log("Height: " + h);
+    //console.log(w);
+    //console.log(canvas.width);
+
+    gamefield.height = size;
+    gamefield.width = size;
+
+    gamefield.spaceBetween = (2*size) / 15;
+    gamefield.circleSize = (2*size) / 120;
+
+    gamefield.lineWidth = (2*size) / 350;
     if (gamefield.circleSize < 1) {
        gamefield.circleSize = 1
     };
-    gamefield.leftOffset = (canvas.width - h) / 2;
+    gamefield.leftOffset = (canvas.width - size) / 2;
 
     console.log(gamefield);
   }
