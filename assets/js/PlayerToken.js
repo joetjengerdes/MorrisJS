285e3a87f4423a8fa8fab1f8c5cf7d2cb4f16b97
omit66 <omit66@gmail.com>
2016-08-23 19:38:20 +0200

    alter uni student

--- assets/js/PlayerToken.js
+++ assets/js/PlayerToken.js
@@ -1,26 +1,14 @@
 function PlayerToken(p) {
-    var player = p;
+    var mPlayer = p;
     var mVertexId = -1;
     var mSelected = false;
 
-    this.move = function(x, y) {
-        return;
-    }
-
-    this.contains = function() {
-        return;
-    }
-
-    this.isVisible = function() {
-        return this.x === 'undefinied' || this.y === 'undefinied';
-    }
-
     this.getColor = function() {
-        return player.color;
+        return mPlayer.color;
     }
 
     this.getPlayer = function() {
-        return player;
+        return mPlayer;
     }
 
     this.isSelected = function() {
