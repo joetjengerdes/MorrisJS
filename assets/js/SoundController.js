0e94caf692c6fda3ef8e9bc45a6dd600ce011d42
Jannis Oetjengerdes <ja_oe@uni-bremen.de>
2016-08-23 21:57:37 +0200

    soundfix

--- assets/js/SoundController.js
+++ assets/js/SoundController.js
@@ -7,10 +7,14 @@ function SoundController() {
     var tokenElem = document.getElementById(TOKEN_STEAL_HTML_ID);
 
     this.playMoveSound = function() {
+        moveElem.pause();
+        moveElem.currentTime = 0;
         moveElem.play();
     }
 
     this.playTokenStealSound = function() {
+        tokenElem.pause();
+        tokenElem.currentTime = 0;
         tokenElem.play();
     }
 
