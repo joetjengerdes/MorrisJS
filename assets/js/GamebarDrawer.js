4524a30c54927382ebe0a672516d5341634773ce
Jannis Oetjengerdes <ja_oe@uni-bremen.de>
2016-08-30 18:19:06 +0200

    nochma

---
+++ assets/js/GamebarDrawer.js
@@ -0,0 +1,52 @@
+function GamebarDrawer(gbar) {
+    var mGamebar = gbar;
+    mGamebar.setTextChangedListener(this);
+    var mSidebar = document.getElementById("sidebar-inner");
+
+    var mGeneralImg = "<img src=\"assets/img/flags.png\"></img>";
+    var mCpuImg = "<img src=\"assets/img/cpu.png\"></img>";
+    var mPlayerImg = "<img src=\"assets/img/player.png\"></img>";
+
+    var mText = "";
+
+
+    this.statusBarTextChanged = function() {
+
+        var mText = "<p>";
+        if (mGamebar.isGeneralEvent()) {
+            mText += mGeneralImg + mGamebar.getText();
+        } else {
+            var p1 = mGamebar.getPlayerActionFrom();
+            var p2 = mGamebar.getPlayerActionAffected();
+
+            console.log(mPlayerImg);
+
+            if (p1) {
+                if (p1.isCPU()) {
+
+                    mText = mText + mCpuImg;
+                } else {
+                    mText = mText + mPlayerImg;
+                }
+            }
+            if (p2) {
+                if (p2.isCPU()) {
+                    mText = mText + mCpuImg;
+                } else {
+                    mText = mText + mPlayerImg;
+                }
+            }
+            if (!p1 && !p2) {
+                return;
+            }
+            mText += mGamebar.getText();
+
+            mText = mText + "</p>";
+        }
+        console.log(mSidebar.innerHTML);
+        console.log(mText);
+        mSidebar.innerHTML += mText;
+        console.log(mSidebar.innerHTML);
+    }
+
+}
