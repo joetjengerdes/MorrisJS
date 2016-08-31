7b4c035bd7afe21a8592e71b5057cd6bd6975f66
Jannis Oetjengerdes <ja_oe@uni-bremen.de>
2016-08-31 14:53:52 +0200

    kommentare Eventcontroller

--- assets/js/EventController.js
+++ assets/js/EventController.js
@@ -1,34 +1,73 @@
+/**
+ * The event controller is events caused by
+ * the user, e.g. resizing the windows or
+ * doing clicks.
+ * @param {HTMLElement} canvas    the canvas of the game
+ * @param {Gamefield} gamefield gamefield of the game
+ */
 function EventController(canvas, gamefield) {
-    var canvas = canvas;
+    // canvas of the game
+    var mCanvas = canvas;
+    // reference of the gamecontroller
     var mGameController = null;
+    // reference of the drawController
     var mDrawController = null;
-    var mSizeCalculationService = new SizeCalculationService(canvas, gamefield);
+    // create service for recalculations of the gamefield
+    var mSizeCalculationService = new SizeCalculationService(mCanvas, gamefield);
     // timeout used for resizing, so calculation
     // is not done by every frame change
     var mResizeTimeout = null;
+    // timeout for mouse moving, so it is not refreshed by every frame
     var mMouseMoveTimeout = null;
+    // flag if the cursor is in range of a token, if it stays there
+    // this variable stays true
     var mCursorInRange = false;
 
+    /**
+     * Sets a gamecontroller
+     * @param {GameController} controller a game controller
+     */
     this.setGameController = function(controller) {
         mGameController = controller;
     }
 
+
+    /**
+     * Gets the game controller
+     * @return {GameController} the gamecontroller
+     */
     this.getGameController = function() {
         return mGameController;
     }
 
+    /**
+     * Sets a drawcontroller
+     * @param {GameController} drawc a drawcontroller
+     */
     this.setDrawController = function(drawc) {
         mDrawController = drawc;
     }
 
+    /**
+     * Gets the drawcontroller
+     * @return {DrawController} the drawcontroller
+     */
     this.getDrawController = function() {
         return mDrawController;
     }
 
+    /**
+     * Gets the service for gamefield calculations
+     * @return {SizeCalculationService} the service
+     */
     this.getSizeCalculationService = function() {
         return mSizeCalculationService;
     }
 
+    /**
+     * initializes the controller, in this function all
+     * handlers are added to the inputs which belong to the game
+     */
     function initController() {
         /*canvas.addEventListener('selectstart', function(e) {
             e.preventDefault();
@@ -95,7 +134,12 @@ function EventController(canvas, gamefield) {
         mSizeCalculationService.calculate();
     }
 
-    //return relative position to canvas
+    /**
+     * This function return the relative mouse-position of
+     * the canvas
+     * @param  {Event} e the event of the mouse
+     * @return {Object}   Object which contains the x and y coordinates
+     */
     function getMouse(e) {
         var x;
         var y;
@@ -106,8 +150,8 @@ function EventController(canvas, gamefield) {
             x = e.clientX + document.body.scrollLeft + document.documentElement.scrollLeft;
             y = e.clientY + document.body.scrollTop + document.documentElement.scrollTop;
         }
-        x -= canvas.offsetLeft;
-        y -= canvas.offsetTop;
+        x -= mCanvas.offsetLeft;
+        y -= mCanvas.offsetTop;
 
         var mouseClick = {
             x: x,
